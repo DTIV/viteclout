a4f08aee250e2a6340e62c0fb2e737a9058d9bc3
DTIV <52395586+DTIV@users.noreply.github.com>
2021-12-16 18:53:55 -0500

    final

--- frontend/src/components/vuilder/ProfilePic.js
+++ frontend/src/components/vuilder/ProfilePic.js
@@ -2,13 +2,23 @@ import React from 'react'
 import emptyProfile from '../empty-profile.png'
 
 const ProfilePic = (props) => {
-    var img = props.profilePic.replace("http://localhost:5000/images/","")
-    if(img){
-        return (
-            <div className="profile-card">
-                <img className="profile-pic" src={props.profilePic} alt="" />
-            </div>
-        )
+    if(props.profilePic){
+        var img = props.profilePic.replace("http://localhost:5000/images/","")
+        if(img){
+            return (
+                <div className="profile-card">
+                    <img className="profile-pic" src={props.profilePic} alt="" />
+                </div>
+        )else{
+            return (
+                <div className="profile-card">
+                    <img className="profile-pic" src={emptyProfile} alt="" />
+                </div>
+            )
+        }
+    }
+    
+    
     }else{
         return (
             <div className="profile-card">
